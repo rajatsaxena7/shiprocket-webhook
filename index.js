d7ddcfb76adfa10ef31d39902b34bbeef22b58e9
Rajat saxena <62472831+rajatsaxena7@users.noreply.github.com>
2024-06-21 13:08:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
 
 
 admin.initializeApp({
